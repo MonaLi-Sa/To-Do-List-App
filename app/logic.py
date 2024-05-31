70c4b45703537d9491166dca958a1066d6e67214
MonaLi-Sa <136165205+MonaLi-Sa@users.noreply.github.com>
2024-05-31 15:25:26 +0530

    Create logic.py

---
+++ app/logic.py
@@ -0,0 +1,12 @@
+# app/logic.py
+
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def remove_task(self, index):
+        if 0 <= index < len(self.tasks):
+            self.tasks.pop(index)
